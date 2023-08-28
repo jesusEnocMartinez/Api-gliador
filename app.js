a6c78287409d89554b2e62825ed3bfe2e631d393
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2023-08-27 20:47:32 -0600

    Update app.js
    Se correfio app, no debe ir haci se buscara una solucion para manejar base

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ async function sincronizarBaseDeDatos() {
     await sequelize.sync();
     console.log('Tablas sincronizadas con la base de datos.');
   } catch (error) {
-    console.error('Error al sincronizar las tablas:', error);
+    console.error('Error al sincronizar las tablas:', error); // Muestra el error en la consola
   }
 }
 
@@ -35,6 +35,17 @@ sincronizarBaseDeDatos();
 // Ruta de ejemplo
 app.get('/', (req, res) => {
   res.send('Backend--API--Gladiador');
+});
+
+// Manejador de ruta para agregar un nuevo usuario
+app.post('/api/v1/Insertar/usuarios', async (req, res) => {
+  try {
+    // Aquí puedes colocar la lógica para agregar el usuario a la base de datos
+    res.json({ mensaje: 'Usuario agregado correctamente' });
+  } catch (error) {
+    console.error('Error al agregar el usuario:', error);
+    res.status(500).json({ mensaje: 'Error al agregar el usuario' });
+  }
 });
 
 app.listen(port, () => {
